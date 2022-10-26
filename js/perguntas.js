7c679f62a9d6460f0788a13bbf42f92d12fc823f
Leonardo Alves <alvesleo2002@gmail.com>
2022-10-26 15:55:53 -0300

    Timer countdown finalizado e botao de sair arrumado js

--- js/perguntas.js
+++ js/perguntas.js
@@ -29,13 +29,26 @@ const questions = [
 
 ]
 
+let question_count = 0;
+const number_question = document.querySelector("span#number-question");
+number_question.innerHTML = question_count;
+
 // Objetos da tela a serem modificados conforme a questão
 const question_image = document.querySelector("img#image-question");
 const question_title = document.querySelector("p#current-question");
 const options = document.querySelectorAll("button.button-option");
-const button_quit = document.querySelector("button.sair-btn")
-const button_next_question = document.querySelector("button.next-btn")
-button_next_question.disabled = true
+const button_quit = document.querySelector("button.sair-btn");
+
+const timer = document.querySelector("span#timer");
+let timer_seconds = 16;
+
+const button_next_question = document.querySelector("button.next-btn");
+button_next_question.disabled = true;
+
+// TENTATIVA DE PEGAR O AFTER DA PROGRESS BAR
+/*const progress_bar = window.getComputedStyle(
+    document.querySelector(".base"), "::after"
+).getPropertyValue("max-width");*/
 
 // Variável que vai sortear a próxima pergunta
 let current_question = Math.floor(Math.random() * 5);
@@ -70,11 +83,26 @@ for (let opt of options) {
     c++
 }
 
-window.onload = nextQuestion()
+window.onload = () => {
+    nextQuestion();
+    countDown();
+}
 
-button_next_question.addEventListener("click", nextQuestion)
+button_next_question.addEventListener("click", () => {
+    timer.innerHTML = 15;
+    timer_seconds = 16;
+    nextQuestion();
+});
 
 function nextQuestion(){
+    if(question_count !== 5){
+        question_count++;
+    } else{
+        question_count = 5;
+    }
+
+    number_question.innerHTML = question_count;
+    console.log(question_count)
     if (perguntas_respondidas.length === questions.length) {
         gameOver()
     }
@@ -108,3 +136,29 @@ function gameOver() {
 
 }
 
+function countDown(){
+    timer_seconds--
+    timer.innerHTML = timer_seconds;
+    if (timer_seconds > 0) {
+		setTimeout(countDown, 1000);
+	} else if(timer_seconds === 0){
+        nextQuestion();
+        timer.innerHTML = 15;
+        timer_seconds = 16;
+        countDown();
+    }
+
+    // TENTATIVA DE REALIZAR COUNTDOWN PROGRESS BAR
+    /*let animation_bar = setInterval(() => {
+        let progress_width = timer_seconds / 10 * 100;
+
+        if (timer_seconds > 0){
+            progress_bar.style.width = progress_width + "%";
+        } else{
+            clearInterval(animation_bar);
+            progress_bar.style.width = progress_bar = "0%";
+        }
+    }, 1000);*/
+
+
+}
