75f4b1336fdd43522232d8e2fdae7cffbe66810f
Pedro Henrique Galvão <70819419+pedrohenriquegalvao@users.noreply.github.com>
2022-10-28 09:54:28 -0300

    Ordenando corretamente a página de ranking.

--- js/melhores_jogadores.js
+++ js/melhores_jogadores.js
@@ -1,51 +1,65 @@
 window.onload = () => {
     const container_table = document.querySelector("div.rank-table")
-    const table = document.querySelector("table#rank")
+    //let table = document.getElementById("#rank")
     const tr_primeiro_lugar = document.querySelector("tr#primeiro-lugar")
-    console.log(tr_primeiro_lugar)
-
 
     let jogadores = JSON.parse(localStorage.getItem("jogadores"))
 
-    console.log(jogadores)
+    console.log("jogadores ", jogadores)
+    
+    ordenados = jogadores.sort((a, b) => (a.pontuacao > b.pontuacao) ? -1 : 1)
 
-    let maior_pontuacao = -99
-    let melhor_jogador = {nome: "a", pontuacao: -999}
-    for (let jogador of jogadores) {
-        if (jogador.pontuacao > maior_pontuacao) {
-            maior_pontuacao = jogador.pontuacao
-            melhor_jogador.nome = jogador.nome
-            melhor_jogador.pontuacao = jogador.pontuacao
-        }     
+    let c = 1
+    for (let jogador of jogadores){
+        let tr_teste = document.createElement("tr")
+        tr_teste.setAttribute("id", `${c}lugar`)
+
+        td_posicao_teste = document.createElement("td")
+        td_posicao_teste.classList.add(`rank${c}`)
+        td_posicao_teste.innerHTML = `${c}`
+
+        td_pontuacao_teste = document.createElement("td")
+        td_pontuacao_teste.innerHTML = `${jogador.pontuacao}`
+
+        td_nome_teste = document.createElement("td")
+        td_nome_teste.innerHTML = `${jogador.nome}`
+
+        tr_teste.appendChild(td_posicao_teste)
+        tr_teste.appendChild(td_pontuacao_teste)
+        tr_teste.appendChild(td_nome_teste)
+
+        console.log("teste: ", tr_teste)
+
+        document.getElementById("rank").appendChild(tr_teste)
+        c++
     }
 
-    container_table.innerHTML += `Melhor jogador ${melhor_jogador.nome}. Pontuação ${melhor_jogador.pontuacao}`
+    // let tr_create = document.createElement("tr");
+    // tr_create.setAttribute("id", "primeirolugar");
 
-    let tr_create = document.createElement("tr");
-    tr_create.setAttribute("id", "primeirolugar");
+    // document.getElementById("rank").prepend(tr_create)
+    
+    // let td_posicao_primeiro_lugar = document.createElement("td")
+    // td_posicao_primeiro_lugar.classList.add("rank1")
+    // td_posicao_primeiro_lugar.innerHTML = 1
 
-    document.getElementById("rank").appendChild(tr_create)
+    // let td_nome_primeiro_lugar = document.createElement("td")
+    // td_nome_primeiro_lugar.setAttribute("id", "nome-primeiro-lugar")
+    // td_nome_primeiro_lugar.innerHTML = jogadores[0].nome
 
-    let td_posicao_primeiro_lugar = document.createElement("td")
-    td_posicao_primeiro_lugar.classList.add("rank1")
-    td_posicao_primeiro_lugar.innerHTML = 1
+    // let td_pont_primeiro_lugar = document.createElement("td")
+    // td_pont_primeiro_lugar.setAttribute("id", "pont-primeiro-lugar")
+    // td_pont_primeiro_lugar.innerHTML = jogadores[0].pontuacao
 
-    let td_nome_primeiro_lugar = document.createElement("td")
-    td_nome_primeiro_lugar.setAttribute("id", "nome-primeiro-lugar")
-    td_nome_primeiro_lugar.innerHTML = melhor_jogador.nome
+    // tr_create.appendChild(td_posicao_primeiro_lugar)
+    // tr_create.appendChild(td_pont_primeiro_lugar)
+    // tr_create.appendChild(td_nome_primeiro_lugar)
 
-    let td_pont_primeiro_lugar = document.createElement("td")
-    td_pont_primeiro_lugar.setAttribute("id", "pont-primeiro-lugar")
-    td_pont_primeiro_lugar.innerHTML = melhor_jogador.pontuacao
+    // console.log(td_posicao_primeiro_lugar)
+    // console.log(td_pont_primeiro_lugar)
+    // console.log(td_nome_primeiro_lugar)
 
-    tr_create.appendChild(td_posicao_primeiro_lugar)
-    tr_create.appendChild(td_pont_primeiro_lugar)
-    tr_create.appendChild(td_nome_primeiro_lugar)
+    // console.log("console tr primeiro lugar: ", tr_create)
 
-    console.log(td_posicao_primeiro_lugar)
-    console.log(td_pont_primeiro_lugar)
-    console.log(td_nome_primeiro_lugar)
-
-    console.log("console tr primeiro lugar: ", tr_create)
 
 }
