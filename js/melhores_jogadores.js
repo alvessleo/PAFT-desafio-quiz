dd5014ea879e7713a788e025dfa7a1ff267fe6a3
Leonardo Alves <alvesleo2002@gmail.com>
2022-10-28 14:49:42 -0300

    ajustes antes de enviar

--- js/melhores_jogadores.js
+++ js/melhores_jogadores.js
@@ -1,6 +1,5 @@
 window.onload = () => {
     const container_table = document.querySelector("div.rank-table")
-    //let table = document.getElementById("#rank")
     const tr_primeiro_lugar = document.querySelector("tr#primeiro-lugar")
 
     let jogadores = JSON.parse(localStorage.getItem("jogadores"))
@@ -33,33 +32,5 @@ window.onload = () => {
         document.getElementById("rank").appendChild(tr_teste)
         c++
     }
-
-    // let tr_create = document.createElement("tr");
-    // tr_create.setAttribute("id", "primeirolugar");
-
-    // document.getElementById("rank").prepend(tr_create)
-    
-    // let td_posicao_primeiro_lugar = document.createElement("td")
-    // td_posicao_primeiro_lugar.classList.add("rank1")
-    // td_posicao_primeiro_lugar.innerHTML = 1
-
-    // let td_nome_primeiro_lugar = document.createElement("td")
-    // td_nome_primeiro_lugar.setAttribute("id", "nome-primeiro-lugar")
-    // td_nome_primeiro_lugar.innerHTML = jogadores[0].nome
-
-    // let td_pont_primeiro_lugar = document.createElement("td")
-    // td_pont_primeiro_lugar.setAttribute("id", "pont-primeiro-lugar")
-    // td_pont_primeiro_lugar.innerHTML = jogadores[0].pontuacao
-
-    // tr_create.appendChild(td_posicao_primeiro_lugar)
-    // tr_create.appendChild(td_pont_primeiro_lugar)
-    // tr_create.appendChild(td_nome_primeiro_lugar)
-
-    // console.log(td_posicao_primeiro_lugar)
-    // console.log(td_pont_primeiro_lugar)
-    // console.log(td_nome_primeiro_lugar)
-
-    // console.log("console tr primeiro lugar: ", tr_create)
-
 
 }
